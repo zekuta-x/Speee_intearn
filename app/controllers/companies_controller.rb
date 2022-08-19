83024c173ed308b7a89f5166d1f01cf2162a44c2
zekuta-x <rem_lim11@icloud.com>
2022-08-19 16:48:53 +0900

    [#4] fix: controllersをCRUD記述に対応

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# CompanyInfoController in ApplicationController
+class CompaniesController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+end
