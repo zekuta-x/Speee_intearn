88805e92a2ed2020a78c5aca1fcdef47c012165e
Naoki Takashima <takat.fb.xavi@gmail.com>
2022-08-19 21:21:12 +0900

    refactor: 不要なコメントを削除
    Style/DocumentationをDisableにしたため削除して問題ない

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module HrEngInternship20221stTeam2
-  class Application < Rails::Application # rubocop:disable Style/Documentation
+  class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
