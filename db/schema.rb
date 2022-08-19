151ce0e9406650311b92a0d52d235550fd4617a0
fairy26 <yoshida-yosei-vx@ynu.jp>
2022-08-19 16:14:46 +0900

    [#6] fix: マイグレーションの適用

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_19_052345) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_19_071221) do
   create_table "assessable_areas", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "store_id"
     t.bigint "municipality_id"
@@ -20,19 +20,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_19_052345) do
     t.index ["store_id"], name: "index_assessable_areas_on_store_id"
   end
 
+  create_table "cities", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.bigint "prefecture_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["prefecture_id"], name: "index_cities_on_prefecture_id"
+  end
+
   create_table "companies", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.integer "ieul_company_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "municipalities", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.bigint "prefecture_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["prefecture_id"], name: "index_municipalities_on_prefecture_id"
   end
 
   create_table "prefectures", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -76,11 +76,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_19_052345) do
     t.index ["store_id"], name: "index_target_property_types_on_store_id"
   end
 
-  add_foreign_key "assessable_areas", "municipalities"
+  add_foreign_key "assessable_areas", "cities", column: "municipality_id"
   add_foreign_key "assessable_areas", "stores"
-  add_foreign_key "municipalities", "prefectures"
+  add_foreign_key "cities", "prefectures"
+  add_foreign_key "stores", "cities", column: "municipality_id"
   add_foreign_key "stores", "companies"
-  add_foreign_key "stores", "municipalities"
   add_foreign_key "target_property_types", "property_types"
   add_foreign_key "target_property_types", "stores"
 end
