256c452c608a01fe4412c8d87d8b7c7e1e79296d
zekuta-x <rem_lim11@icloud.com>
2022-08-19 16:48:53 +0900

    [#4] fix: controllersをCRUD記述に対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'company_info/company_page'
-  # root 'company_info#company_page'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  resources :companies, only: [:index, :show]
 end
