c6147265b9f32e09ab1da3d86bfe8cfd72d9dbec
Naoki Takashima <takat.fb.xavi@gmail.com>
2022-08-19 21:21:12 +0900

    refactor: 不要なコメントを削除
    Style/DocumentationをDisableにしたため削除して問題ない

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-# CompanyInfoController in ApplicationController
 class CompaniesController < ApplicationController
   def index; end
   def show; end
--- test/controllers/companies_controller_test.rb
+++ test/controllers/companies_controller_test.rb
@@ -2,7 +2,6 @@
 
 require 'test_helper'
 
-# CompaneisControllerのrootingTest
 class CompaniesControllerTest < ActionDispatch::IntegrationTest
   test 'should get companies page' do
     get company_url(1)
