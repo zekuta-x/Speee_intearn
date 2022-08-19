58d5ced1db6ec8dd48d379b142e9808960205b77
fairy26 <yoshida-yosei-vx@ynu.jp>
2022-08-19 16:41:35 +0900

    [#6] fix: カラムに非NULL制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_19_072919) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_19_073538) do
   create_table "assessable_areas", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "store_id"
     t.bigint "municipality_id"
@@ -30,19 +30,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_19_072919) do
   end
 
   create_table "branchs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.bigint "company_id", null: false
-    t.integer "ieul_branch_id"
-    t.string "logo_url"
+    t.integer "ieul_branch_id", null: false
+    t.string "logo_url", null: false
     t.string "post_code"
     t.bigint "municipality_id", null: false
-    t.string "property_address"
+    t.string "property_address", null: false
     t.string "phone_number"
     t.string "fax_number"
     t.string "open_hours"
     t.string "regular_holiday"
-    t.text "catch_copy"
-    t.text "introduction"
+    t.text "catch_copy", null: false
+    t.text "introduction", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["company_id"], name: "index_branchs_on_company_id"
@@ -50,7 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_19_072919) do
   end
 
   create_table "cities", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.bigint "prefecture_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -58,20 +58,20 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_19_072919) do
   end
 
   create_table "companies", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.integer "ieul_company_id"
+    t.string "name", null: false
+    t.integer "ieul_company_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "prefectures", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "property_types", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
