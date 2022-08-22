08903be121b2ca3630ff92828125948ea133920f
fairy26 <yoshida-yosei-vx@ynu.jp>
2022-08-22 11:11:20 +0900

    [#56] add: 企業Modelのvalidationを追加
    非NULL制約 / ユニーク制約

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Company < ApplicationRecord
+  valiadtes :name, presence: true, uniqueness: true
+  valiadtes :ieul_company_id, presence: true, uniqueness: true
 end
