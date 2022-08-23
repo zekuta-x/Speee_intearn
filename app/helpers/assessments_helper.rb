07333d53bdf711663afde15f8964871ed63e455e
zekuta-x <rem_lim11@icloud.com>
2022-08-23 19:13:29 +0900

    [#30] fix: enumと書き方を統一

--- app/helpers/assessments_helper.rb
+++ app/helpers/assessments_helper.rb
@@ -2,9 +2,17 @@
 
 module AssessmentsHelper
   def room_plans
-    [['1R(ワンルーム)', '1'], ['1K/1DK', '2'], ['2K/2DK', '3'],
-     ['2LK/2LDK', '4'], ['3K/3DK', '5'], ['3LK/3LDK', '6'],
-     ['4K/4DK', '7'], ['5K/5DK', '8'], ['5LK/5LDK', '9'],
-     ['6K/6DK', '10'], ['6LK/6LDK以上', '11']]
+    { '1R(ワンルーム)': 1, '1K/1DK': 2, '2K/2DK': 3,
+      '2LK/2LDK': 4, '3K/3DK': 5, '3LK/3LDK': 6,
+      '4K/4DK': 7, '5K/5DK': 8, '5LK/5LDK': 9,
+      '6K/6DK': 10, '6LK/6LDK以上': 11 }
+  end
+
+  def type
+    { '分譲マンション': 1, '一戸建て': 2, '土地': 3 }
+  end
+
+  def area_unit
+    { '平方メートル': 1, '坪': 2 }
   end
 end
