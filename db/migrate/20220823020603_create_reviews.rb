649e9add59f17bf4427620512d5e06f13277d1c3
Matama091 <matama@pluslab.org>
2022-08-23 18:18:26 +0900

    [#15] fix review column name
    アドバイスと改善のカラムを修正

--- db/migrate/20220823020603_create_reviews.rb
+++ db/migrate/20220823020603_create_reviews.rb
@@ -27,8 +27,8 @@ class CreateReviews < ActiveRecord::Migration[7.0]
       t.text :concerns_about_the_sale, null: false
       t.text :reason_for_company_selection, null: false
       t.text :reason_for_response_satisfaction, null: false
-      t.text :advice, null: false
-      t.text :improvement
+      t.text :advice_on_Selling, null: false
+      t.text :improvement_of_company
 
       t.timestamps
     end
