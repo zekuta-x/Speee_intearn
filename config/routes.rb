f7307f6e4812f96f398cff90d9ec6ed17017ae00
Matama091 <matama@pluslab.org>
2022-08-23 20:59:19 +0900

    [#33] fix routes
    ルートを編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@
 
 Rails.application.routes.draw do
   # Defines the root path route ("/")
-<<<<<<< HEAD
   resources :companies, only: %i[show]
   resources :assessments, only: %i[new create show]
-=======
-  resources :branches, only: %i[show]
-  resources :assessments, only: %i[new create]
->>>>>>> main
 end
