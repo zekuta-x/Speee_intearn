6abc3f49acc14b99d39f011122fc92abaf204b88
zekuta-x <rem_lim11@icloud.com>
2022-08-23 17:41:18 +0900

    [#30] fix: helper, tag変更

---
+++ app/helpers/assessments_helper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module AssessmentsHelper
+    def room_plans
+        [['1R(ワンルーム)', '1'], ['1K/1DK', '2'], ['2K/2DK', '3'],
+                                  ['2LK/2LDK', '4'], ['3K/3DK', '5'], ['3LK/3LDK', '6'],
+                                  ['4K/4DK', '7'], ['5K/5DK', '8'], ['5LK/5LDK', '9'],
+                                  ['6K/6DK', '10'], ['6LK/6LDK以上', '11']]
+    end
+end
