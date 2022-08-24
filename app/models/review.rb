37e4e91a5e170de66a6ed9e83756defe283047c2
Matama091 <matama@pluslab.org>
2022-08-24 14:46:43 +0900

    [#15] fix: 口コミモデルのカラムの名前を変更
    口コミテーブルの名前をsale・sold_outで統一し、修正した

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class Review < ApplicationRecord
-  enum number_of_sales: { first: 0, second: 1, after_the_third_time: 2 }, _prefix: true
+  enum number_of_sale: { first: 0, second: 1, after_third_time: 2 }, _prefix: true
   enum reason_for_sale: { resettlement: 0, inheritance: 1, changing_job: 2, divorce: 3, asset_liquidation: 4,
                           financial_reasons: 5, other: 6 }
-  enum brokerage_contract: { exclusive_duty: 0, fulltime_mediation: 1, general_mediation: 2, unknown: 3 }
+  enum contract_category: { exclusive_duty: 0, fulltime_mediation: 1, general_mediation: 2, unknown: 3 }
 
   belongs_to :branch
   belongs_to :city
@@ -14,9 +14,9 @@ class Review < ApplicationRecord
   validates :first_name, presence: true
   validates :age, presence: true
   validates :other_address, presence: true
-  validates :response_satisfaction, presence: true,
+  validates :customer_satisfaction, presence: true,
                                     numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
-  validates :number_of_sales, presence: true
+  validates :number_of_sale, presence: true
   validates :reason_for_sale, presence: true
   validates :sale_consideration_period, presence: true
   validates :assessment_request_period, presence: true
@@ -24,13 +24,12 @@ class Review < ApplicationRecord
   validates :sold_out_period, presence: true
   validates :delivery_period, presence: true
   validates :appraisal_price, presence: true
-  validates :sold_price, presence: true
+  validates :sale_price, presence: true
   validates :contract_price, presence: true
-  validates :price_reduction_flag, presence: true
   validates :contract_category, presence: true
   validates :review_title, presence: true
-  validates :concerns_about_the_sale, presence: true
+  validates :concern_about_sale, presence: true
   validates :reason_for_company_selection, presence: true
   validates :reason_for_response_satisfaction, presence: true
-  validates :advice_on_selling, presence: true
+  validates :advice_on_sale, presence: true
 end
--- db/migrate/20220823020603_create_reviews.rb
+++ db/migrate/20220823020603_create_reviews.rb
@@ -7,8 +7,8 @@ class CreateReviews < ActiveRecord::Migration[7.0]
       t.integer :age, null: false
       t.references :city, null: false, foreign_key: true
       t.string :other_address, null: false
-      t.integer :response_satisfaction, null: false
-      t.integer :number_of_sales, null: false, default:0
+      t.integer :customer_satisfaction, null: false
+      t.integer :number_of_sale, null: false, default:0
       t.integer :reason_for_sale, null: false, default:0
       t.references :property_type, null: false, foreign_key: true
       t.date :sale_consideration_period, null: false
@@ -17,17 +17,17 @@ class CreateReviews < ActiveRecord::Migration[7.0]
       t.date :sold_out_period, null: false
       t.date :delivery_period, null: false
       t.integer :appraisal_price, null: false
-      t.integer :sold_price, null: false
+      t.integer :sale_price, null: false
       t.integer :contract_price, null: false
       t.integer :price_reduction_time
       t.integer :price_reduction
       t.integer :contract_category, null: false, default:0
       t.string :review_title, null: false
-      t.text :concerns_about_the_sale, null: false
+      t.text :concern_about_sale, null: false
       t.text :reason_for_company_selection, null: false
-      t.text :reason_for_response_satisfaction, null: false
-      t.text :advice_on_selling, null: false
-      t.text :improvement_of_company
+      t.text :reason_for_customer_satisfaction, null: false
+      t.text :advice_on_sale, null: false
+      t.text :improvement_to_company
 
       t.timestamps
     end
