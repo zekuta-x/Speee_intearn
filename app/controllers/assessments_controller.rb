0b2b5dd0154a7015c2750140779857b223959132
Matama091 <matama@pluslab.org>
2022-08-24 16:37:01 +0900

    [#33] fix: 変数
    queryの修正

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -18,8 +18,8 @@ class AssessmentsController < ApplicationController
 
   def assessment_params # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
     {
-      'branch_id' => branch_id,
-      'property_city' => property_city,
+      'branch_id' => @branch_id,
+      'property_city' => @property_city,
       'property_address' => params.require(:property_address),
       'property_type' => params.require(:property_type),
       'property_exclusive_area' => params.require(:property_exclusive_area),
@@ -27,7 +27,7 @@ class AssessmentsController < ApplicationController
       'property_building_area_float' => params.require(:property_building_area_float),
       'property_building_area_unit' => params.require(:property_building_area_unit),
       'property_floor_area' => params.require(:property_floor_area),
-      'url_param' => host,
+      'url_param' => @host,
       'property_room_plan' => params.require(:property_room_plan),
       'property_constructed_year' => params.require(:property_constructed_year),
       'user_email' => params.require(:user_email),
