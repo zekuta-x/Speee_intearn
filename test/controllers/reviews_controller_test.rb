53bbe8bb2de312ef662618d950d3d51c0a936b04
fairy26 <yoshida-yosei-vx@ynu.jp>
2022-08-25 20:50:57 +0900

    [#81] refctor: テストの説明を日本語に修正

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class ReviewsControllerTest < ActionDispatch::IntegrationTest
-  test 'should get show' do
+  test '名前付きルートに遷移できる' do
     get reviews_show_url
     assert_response :success
   end
