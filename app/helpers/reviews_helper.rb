35485a5a055050a8e0bf339dae5df3ecf391ed05
fairy26 <yoshida-yosei-vx@ynu.jp>
2022-08-26 19:13:42 +0900

    [#93] add: 金額を整形するメソッドを追加

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -4,4 +4,8 @@ module ReviewsHelper
   def formatted(date)
     date.strftime('%Y年%m月')
   end
+
+  def formatted_with_unit(value)
+    number_to_currency(value/(10**4).round, locale: 'ja')
+  end
 end
