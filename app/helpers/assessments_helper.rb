b30abaa87047080f02b38541ceeb8e46321819d0
Matama091 <matama@pluslab.org>
2022-08-29 13:48:16 +0900

    [#27]  fix: selectの表示をヘルパーでの表示に変更

--- app/helpers/assessments_helper.rb
+++ app/helpers/assessments_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module AssessmentsHelper
-  def room_plans
+  def property_room_plan
     { '1R(ワンルーム)': 1, '1K/1DK': 2, '1LK/1LDK': 3, '2K/2DK': 4,
       '2LK/2LDK': 5, '3K/3DK': 6, '3LK/3LDK': 7,
       '4K/4DK': 8, '4LK/4LDK': 9, '5K/5DK': 10, '5LK/5LDK': 11,
@@ -12,7 +12,7 @@ module AssessmentsHelper
     { '分譲マンション': 1, '一戸建て': 2, '土地': 3 }
   end
 
-  def area_unit
+  def property_building_area_unit
     { '平方メートル': 1, '坪': 2 }
   end
 end
