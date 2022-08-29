48bdec15ee9b88a1e5352f65e74a5d4caa705e01
zekuta-x <rem_lim11@icloud.com>
2022-08-29 16:42:47 +0900

    [#109] fix: ルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'prefectures#index'
   # Defines the root path route ("/")
   resources :branches, only: %i[show]
   resources :assessments, only: %i[new create show]
   resources :reviews, only: %i[show]
   resources :cities, only: %i[show]
-  resources :homes, only: %i[index]
 end
