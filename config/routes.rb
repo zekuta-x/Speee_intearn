e7e20fcea7d58adcf609667e5d0c52a0c68311f8
Matama091 <matama@pluslab.org>
2022-08-29 20:17:20 +0900

    [#27]  fix: Conflictの修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   resources :branches, only: %i[show]
   resources :assessments, only: %i[new create show]
+  resources :reviews, only: %i[show]
+  resources :cities, only: %i[show]
+
+  # 【ヘルスチェック用】
+  get '/ops/heartbeat', to: 'assessments#new'
+  # 【査定依頼フォームでのリダイレクト用】
   get '/assessments', to: redirect('/assessments/new')
 end
