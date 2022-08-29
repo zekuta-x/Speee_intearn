501afc1f5a0270c1de14b6de1f20431ece6f30d1
fairy26 <yoshida-yosei-vx@ynu.jp>
2022-08-29 12:05:23 +0900

    [#110] add: 口コミの並び順をパラメータで変更してインスタンスで持つように変更

--- app/controllers/branches_controller.rb
+++ app/controllers/branches_controller.rb
@@ -3,5 +3,12 @@
 class BranchesController < ApplicationController
   def show
     @branch = Branch.find(params[:id])
+    if params[:latest]
+      @reviews = @branch.reviews.latest
+    elsif params[:best]
+      @reviews = @branch.reviews.best
+    else
+      @reviews = @branch.reviews
+    end
   end
 end
