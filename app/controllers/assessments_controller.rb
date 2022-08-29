766116f6eb65a2ede360f9d7c6928b727c0e3a68
Matama091 <matama@pluslab.org>
2022-08-29 14:50:51 +0900

    [#27]  refactor: Lint修正

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -13,19 +13,16 @@ class AssessmentsController < ApplicationController
     @assessment_form.url_param = request.host
     if @assessment_form.save
       response = @assessment_form.post_api
-      if response.status == 200
-        redirect_to '/assessments/1', notice: t('.ok')
-      end
+      redirect_to '/assessments/1', notice: t('.ok') if response.status == 200
     else
       redirect_to '/assessments/new', notice: t('.error')
     end
-
   end
 
   private
 
-
-  def temp_param 
-      params.permit(:branch_id, :property_prefecture, :property_city, :property_other_address, :property_type, :property_exclusive_area, :property_land_area, :property_building_area, :property_building_area_unit, :property_floor_area, :property_room_plan, :property_constructed_year, :user_email, :user_first_name, :user_last_name, :user_first_name_kana, :user_last_name_kana, :user_tel)
+  def temp_param
+    params.permit(:branch_id, :property_prefecture, :property_city, :property_other_address, :property_type,
+                  :property_exclusive_area, :property_land_area, :property_building_area, :property_building_area_unit, :property_floor_area, :property_room_plan, :property_constructed_year, :user_email, :user_first_name, :user_last_name, :user_first_name_kana, :user_last_name_kana, :user_tel)  # rubocop:disable Style/LineLength
   end
 end
--- app/models/assessment_form.rb
+++ app/models/assessment_form.rb
@@ -1,26 +1,28 @@
+# frozen_string_literal: true
+
 class AssessmentForm
   include ActiveModel::Model
   include ActiveModel::Attributes
   extend Enumerize
 
-  enumerize :property_room_plan, in:  { r1: 1, k1_dk1: 2, lk2_ldk2: 3, k2_dk2: 4,
-    ld2_ldk2: 5, k3_dk3: 6, lk3_ldk3: 7,
-    k4_dk4: 8, lk4_ldk4: 9, k5_dk5: 10, lk5_ldk5: 11,
-    k6_dk6: 12, others: 13 }
-  enumerize :property_building_area_unit, in: { m2: 1, tsubo: 2}
+  enumerize :property_room_plan, in: { r1: 1, k1_dk1: 2, lk2_ldk2: 3, k2_dk2: 4,
+                                       ld2_ldk2: 5, k3_dk3: 6, lk3_ldk3: 7,
+                                       k4_dk4: 8, lk4_ldk4: 9, k5_dk5: 10, lk5_ldk5: 11,
+                                       k6_dk6: 12, others: 13 }
+  enumerize :property_building_area_unit, in: { m2: 1, tsubo: 2 }
   enumerize :property_type, in: { condominium: 1, house: 2, land: 3 }
 
   attribute :branch_id, :integer
   attribute :property_prefecture, :string
   attribute :property_city, :string
   attribute :property_other_address, :string
-  attribute :property_type, :integer
+  attribute :property_type, :integer, default: 1
   attribute :property_exclusive_area, :integer
   attribute :property_land_area, :integer
   attribute :property_building_area, :integer
-  attribute :property_building_area_unit, :integer
+  attribute :property_building_area_unit, :integer, default: 1
   attribute :property_floor_area, :integer
-  attribute :property_room_plan, :integer
+  attribute :property_room_plan, :integer, default: 1
   attribute :property_constructed_year, :integer
   attribute :url_param, :string
   attribute :user_email, :string
@@ -32,7 +34,7 @@ class AssessmentForm
 
   VALID_NUMBER_REGEX = /\A0\d{1,4}-\d{1,4}-\d{3,4}\z/
 
-  validates :branch_id, presence:true
+  validates :branch_id, presence: true
   validates :property_prefecture, presence: true
   validates :property_city, presence: true
   validates :property_other_address, presence: true
@@ -41,30 +43,35 @@ class AssessmentForm
   validates :property_land_area, presence: true
   validates :property_building_area, presence: true
   validates :property_building_area_unit, presence: true
-  validates :property_floor_area, presence:true
-  validates :property_room_plan, presence:true
+  validates :property_floor_area, presence: true
+  validates :property_room_plan, presence: true
   validates :property_constructed_year, presence: true
   validates :url_param, presence: true
-  validates :user_email, format: { with: URI::MailTo::EMAIL_REGEXP}, length: { maximum: 100 }, presence: true
-  validates :user_first_name, length: { maximum: 15}, presence: true
-  validates :user_last_name, length: { maximum: 15}, presence: true
-  validates :user_first_name_kana, length: { maximum: 31}, presence: true
-  validates :user_last_name_kana, length: {maximum: 31}, presence: true
+  validates :user_email, format: { with: URI::MailTo::EMAIL_REGEXP }, length: { maximum: 100 }, presence: true
+  validates :user_first_name, length: { maximum: 15 }, presence: true
+  validates :user_last_name, length: { maximum: 15 }, presence: true
+  validates :user_first_name_kana, length: { maximum: 31 }, presence: true
+  validates :user_last_name_kana, length: { maximum: 31 }, presence: true
   validates :user_tel, format: { with: VALID_NUMBER_REGEX }, presence: true
 
   def save
     return false unless valid?
-    
+
     true
   end
 
-  def  get_address
+  def address_search
     prefecture = Prefecture.find_by(name: property_prefecture)
     @property_city = prefecture.cities.find_by(name: property_city)
     @property_address = @property_city.name_with_prefecture + property_other_address
+  end
 
-    @user_name = user_last_name + ' ' + user_first_name
-    @user_name_kana = user_last_name_kana + ' ' + user_first_name_kana
+  def name_join
+    "#{user_last_name} #{user_first_name}"
+  end
+
+  def name_kana_join
+    "#{user_last_name_kana} #{user_first_name_kana}"
   end
 
   def post_api_params # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
@@ -72,29 +79,28 @@ class AssessmentForm
       'branch_id' => branch_id,
       'property_city' => @property_city.id,
       'property_address' => @property_address,
-      'property_type' => property_type,
+      'property_type' => property_type.value,
       'property_exclusive_area' => property_exclusive_area,
       'property_land_area' => property_land_area,
       'property_building_area' => property_building_area,
-      'property_building_area_unit' => property_building_area_unit,
+      'property_building_area_unit' => property_building_area_unit.value,
       'property_floor_area' => property_floor_area,
       'url_param' => url_param,
-      'property_room_plan' => property_room_plan,
+      'property_room_plan' => property_room_plan.value,
       'property_constructed_year' => property_constructed_year,
       'user_email' => user_email,
-      'user_name' => @user_name,
-      'user_name_kana' => @user_name_kana,
+      'user_name' => name_join,
+      'user_name_kana' => name_kana_join,
       'user_tel' => user_tel
     }
   end
+
   def post_api
-    get_address
-    pp post_api_params
+    address_search
     conn = Faraday.new(
       url: ENV.fetch('API_URI', nil), params: post_api_params, ssl: { verify: false }
     )
 
-    response = conn.post ENV.fetch('BODY', nil)
-    return response
+    conn.post ENV.fetch('BODY', nil)
   end
 end
