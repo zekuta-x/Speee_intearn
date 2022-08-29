6e20ced8e113b4aacc789a1eafefe6c22d37bef7
Matama091 <matama@pluslab.org>
2022-08-29 16:58:59 +0900

    [#27]  fix: 査定依頼のリダイレクトページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   resources :branches, only: %i[show]
   resources :assessments, only: %i[new create show]
+  get '/assessments', to: redirect('/assessments/new')
 end
